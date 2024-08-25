fa50a497348f753ea97f382b4f6b437234bc7d92
Viraj Gunjal <83692215+virajgunjal@users.noreply.github.com>
2024-08-26 00:57:32 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,73 @@
+// console.log("Sup");
+// let name ="Zeus";
+// alert("Sup "+name);
+
+// let age = 21;
+// console.log(age+name);
+// x = null;
+// y= undefined;
+
+const student ={
+    s_age: 21,
+    s_Name: "Zee",
+    gpa: 8.66,
+    isPass: true
+};
+// console.log(student.s_Name);
+
+// const product = {
+//     title: "Pen",
+//     rating: 4.2,
+//     offer: 5,
+//     price: 420
+// };
+// let a=8;
+// let  b = 9;
+// console.log(a+b);
+
+
+
+//let namer = prompt("enter name");
+//console.log("heyy "+namer);
+
+// let num = prompt("Enter number ");
+// if(num%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+// for(let i=1;i<=num;i++){
+//     console.log("Hey  times");
+// }
+
+// let str = "ZeusJodd";
+// for(let i of str){
+//     console.log("i= "+i);
+// }
+
+// for(let i in student){
+//     console.log(student[i]);
+// }
+
+
+// let str = "HeyZeus";
+
+// console.log(str.length);
+// console.log(str[0]);
+
+// //Templet literals
+
+// let specialstring = `This is templet literal here we print any obj element like this ${student.s_Name} or even a variable like this ${str}`;
+
+// console.log(specialstring);
+// let lstring = "          This string has spaces in start n end and will be removed         "
+// console.log(lstring.trim());
+
+let heros =["hulk", "deadpool","logan"];
+console.log(heros);
+
+const arrowsum = (a,b)=>{
+    return a+b;
+}
